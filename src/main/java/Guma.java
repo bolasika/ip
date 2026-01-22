405916d11e29ad1acf0e84ee69e5defc323a9655
yikleong <yikleong1101@gmail.com>
2026-01-23 01:46:13 +0800

    Added javadocs comments

--- src/main/java/Guma.java
+++ src/main/java/Guma.java
@@ -1,24 +1,49 @@
+import java.util.Scanner;
 public class Guma {
 
-    // Constructor
+    /**
+     * The Constructor for Guma chatbot
+     */
     public Guma() {}
 
-    public String Start() {
+    /**
+     * A greeting message displayed when the chatbot starts
+     *
+     * @return A string representation for introduction
+     */
+    private String Start() {
         return "____________________________________________________________\n" +
                 "Hello! I'm Guma\n" +
                 "What can I do for you?\n" +
                 "____________________________________________________________";
     }
 
-    public String End() {
+    /**
+     * A farewell message displayed when the chatbot ends
+     *
+     * @return A string representation for farewell
+     */
+    private String End() {
         return "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
     }
 
+    /**
+     * Run the chatbot session
+     * Print the Greeting message, then
+     * Print the farewell message
+     */
+    public void run() {
+        System.out.println(this.Start());
+        System.out.println(this.End());
+    }
+
+    /**
+     * Creates Guma chatbot instance and starts the session.
+     */
     public static void main(String... args) {
         Guma bot = new Guma();
-        System.out.println(bot.Start());
-        System.out.println(bot.End());
+        bot.run();
     }
 }
 
